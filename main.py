f556973e304787ea3604350bea837ae3ad2131ec
Xerefic <archish17@gmail.com>
2018-12-31 21:58:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+I am not a listener of Music
+ Don't Judge me. LOL
+"""
+# Strings
+Song = "Shape Of You"
+Artist = "Ed Sheeran"
+Album = "Shape of You"
+Released = "January 2017"
+#Integers
+Recorded = 2016
+Duration = 3*60+53 #In Seconds
+
+#Printing the variables
+print(Song)
+print(Artist)
+print(Album)
+print(Released)
+print(Recorded)
+print(Duration)
